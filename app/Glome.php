32ae7c88cebcd6d4f214fb84baf2253aaad4d2a6
Ferenc Szekely <ferenc.szekely@gmail.com>
2015-12-14 01:30:55 +0200

    bit of refactoring

--- app/Glome.php
+++ app/Glome.php
@@ -2,50 +2,81 @@
 
 namespace App;
 
+use Log;
+use Curl\Curl;
 use Illuminate\Database\Eloquent\Model;
 
 class Glome extends Model
 {
-    
+    private static function getGlomeUrl($entry, $glomeid = null)
+    {
+        $type = 'post';
+        $ret = env('GLOME_SERVER');
+
+        switch($entry)
+        {
+            case 'accounts_create':
+                $ret .= '/accounts/create';
+                break;
+            case 'accounts_show':
+                $type = 'get';
+                if ($glomeid) {
+                    $ret .= '/accounts/' . $glomeid . '/show';
+                }
+                break;
+            default:
+                $ret = '';
+        }
+
+        if ($ret != '' and $type == 'get')
+        {
+            $ret .= '?apikey=' . env('GLOME_APIKEY') . '&apiuid=' . env('GLOME_APIUID');
+        }
+
+        Log::debug('Glome::getGlomeUrl: ' . $ret);
+        return $ret;
+    }
+
     public static function createGlomeAccount()
     {
         $data = [
-            'apikey' => env('GLOME_APIKEY', '09a99d2393d103bb5409e5d72817184f'),
-            'apiuid' => env('GLOME_APIUID', 'GarbagePlanet.20151116150220'),
+            'apikey' => env('GLOME_APIKEY'),
+            'apiuid' => env('GLOME_APIUID'),
         ];
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://api.glome.me/accounts/create');
+        curl_setopt($curl, CURLOPT_URL, Glome::getGlomeUrl('accounts_create'));
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-     
+
         // Timeout in seconds
         curl_setopt($curl, CURLOPT_TIMEOUT, 10);
 
-        $output = curl_exec($curl);
+        $resp = curl_exec($curl);
         curl_close($curl);
 
-        $outputArray = json_decode($output);
-        return $outputArray['glomeid'];
+        Log::debug('Glome::createGlomeAccount: ' . $resp);
+
+        return $resp;
     }
 
-    public static function showGlomeAccount($glomeid)
+    public static function showGlomeAccount($glomeid = null)
     {
-        $data = [
-            'apikey' => env('GLOME_APIKEY', '09a99d2393d103bb5409e5d72817184f'),
-            'apiuid' => env('GLOME_APIUID', 'GarbagePlanet.20151116150220'),
-        ];
+        if ($glomeid == '' or $glomeid == null) return null;
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'https://api.glome.me/accounts/'.$glomeid.'/show?apikey=09a99d2393d103bb5409e5d72817184f&apiuid=GarbagePlanet.20151116150220',
+            CURLOPT_URL => Glome::getGlomeUrl('accounts_show', $glomeid)
         ));
 
         $resp = curl_exec($curl);
         curl_close($curl);
         $outputArray = json_decode($resp, true);
-        return $outputArray['glomeid'];
+
+        Log::debug('Glome::showGlomeAccount: ' . $resp);
+
+        return $resp;
     }
 }
